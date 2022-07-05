8d48f904c9cca2cbc23a3e839b10811dcffc387e
Unlucky117 <ntran343@yahoo.de>
2022-07-05 23:52:00 +0200

    Lösungen

---
+++ Praktikumsaufgabe 5 Felder/Praktikumsaufgabe 5 Felder/Praktikumsaufgabe Felder/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace PraktikumsaufgabeFelder
+{
+    class Program
+    {
+
+        static int[] PascalDreieck(int[] f)                                                   //Rechnung
+        {
+
+            if (f == null)
+                return new int[] { 1 };
+            int[] a = new int [f.Length+1];                                 //Array a erstellen nach Länge von f
+
+            a[0] = 1;                                                       //erste Stelle 1
+            a[f.Length] = 1;                                                //letzte Stelle 1
+            
+            for (int i = 1; i < f.Length; i++)                              
+            {
+                a[i] = f[i-1] + f[i];                                       //Zusammenrechnung der obigen Zahlen im Dreieck
+            }
+
+            return a;
+        }
+
+        static void Ausgabe(int[] f)                                                          //Ausgabe
+        {
+            for (int i = 0; i < f.Length; i++)
+            {
+                Console.Write(f[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static int Primzahlen(int n, bool ausgabe = false)                                    //Primzahlen nach Sieb des Eratosthenes, n muss größer als 0 sein
+        {
+            int anzahlprimzahl = 0;
+            bool[] p = new bool[n];                                                           //festlegung eines Arrays p mit bool
+
+            p[0] = true;                                                                      //1 ist keine Primzahl
+
+            for(int i = 0; i < n; i++)
+            {
+                if (p[i] == false)                                                            //Alle Primzahlen komm mit false an                        
+                {
+                    anzahlprimzahl++;
+                    int vielfache = i+1;
+
+                    if (ausgabe == true)
+                    Console.Write(vielfache+" ");
+
+                    while ( vielfache+i+1 <= n)                                               //Markiere alle vielfachen der Primzahl
+                    {
+                        vielfache += i+1;
+                        p[vielfache-1] = true;
+
+                    }
+                }
+                   
+            }
+            Console.WriteLine();
+            return anzahlprimzahl;
+
+        }
+
+        static void Main(string[] args)                                                       //Vorgegebene Mainfunktion
+        {
+            Console.WriteLine("Pascalsches Dreieck:");
+            int[] f = null;
+            for (int i = 0; i < 8; i++)
+            {
+                f = PascalDreieck(f);
+                Ausgabe(f);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Primzahlen bis 20");
+            Primzahlen(20, true);
+            const int Primzahlgrenze = 750000;
+            Console.WriteLine($"Anzahl der Primzahlen bis {Primzahlgrenze}: {Primzahlen(Primzahlgrenze)}");
+        }
+    }
+}
+
---
+++ Praktikumsaufgabe Black Jack/Praktikumsaufgabe Black Jack/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace Praktikumsaufgabe_Black_Jack
+{
+    class Program
+    {
+        static int KarteZiehen()
+        {
+            Random zufall = new Random();
+            int eineKarte = zufall.Next(2, 12);
+            return eineKarte;
+        }                                                                                           //Funktion für eine zufällige Karte Ziehen
+
+        static void ComputerGewinnt(int chipsguthaben)
+        {
+
+            Console.WriteLine("Ich habe gewonnen.");
+            Console.WriteLine();
+            Console.WriteLine($"Dein Guthaben: {chipsguthaben} Chips");
+        }                                                                                           //Funktion  wenn der Computer gewinnt
+
+        static int ComputerVerliert(int chipsguthaben, int chipseinsatz)
+        {
+
+            Console.WriteLine("Du hast gewonnen.!");
+            Console.WriteLine();
+            chipsguthaben = chipsguthaben + chipseinsatz * 2;
+            Console.WriteLine($"Dein Guthaben: {chipsguthaben} Chips");
+            return chipsguthaben;
+        }                                                                                           //Funktion wenn der Computer verliert
+
+
+        static void Main(string[] args)
+        {
+
+            int chipseinsatz, chipsguthaben, spielergesamt, computergesamt, Karte;
+            chipsguthaben = 10;
+            bool Laufend = true;                                                                    // Variable für Spiel Läuft weiter
+
+            while (Laufend)                                                                             
+
+            {
+                spielergesamt = 0;
+                computergesamt = 0;
+
+                Console.WriteLine("Willkommen bei Black Jack!");
+                Console.WriteLine($"Dein Guthaben beträgt {chipsguthaben} Chips");
+                Console.WriteLine("Wie viele Chips willst du einsetzen?:");
+                chipseinsatz = Convert.ToInt32(Console.ReadLine());
+
+                while (chipseinsatz > chipsguthaben)                                                        //Fehler falls Spieler mehr wetten will als er überhaupt besitzt
+                {
+                    Console.WriteLine($"Dein Einsatz: {chipseinsatz}");
+                    Console.WriteLine($"Du musst mindestens 1 und kannst max. {chipsguthaben} Chips setzen.");
+                    chipseinsatz = Convert.ToInt32(Console.ReadLine());
+                }
+
+                chipsguthaben = chipsguthaben - chipseinsatz;
+
+                for (int i = 0; i < 2; i += 1)                                                          //Spieler zieht 2 Karten und werden aufsummiert.
+                {
+                    Karte = KarteZiehen();
+                    spielergesamt += Karte;
+                    Console.WriteLine($"Karte: {Karte,2} Gesamt: {spielergesamt,2}");
+
+                }
+
+                if (spielergesamt <21 )
+                Console.WriteLine("Noch eine Karte (j/n)?");                                           //Abfrage für noch eine Karte
+                string Eingabe = Console.ReadLine();
+
+                while (spielergesamt < 21 && (Eingabe == "J" || Eingabe == "j"))                        //Bricht ab wenn 21 überschritten oder n/N getippt wird
+                {
+                    Karte = KarteZiehen();
+                    spielergesamt += Karte;
+                    Console.WriteLine($"Karte: {Karte,2} Gesamt: {spielergesamt,2}");
+
+                    if (spielergesamt < 21)
+                    {
+                        Console.WriteLine("Noch eine Karte (j/n)?");
+                        Eingabe = Console.ReadLine();
+                    }
+
+                }
+
+                if (spielergesamt <= 21)                                                            
+                {
+                    Console.WriteLine("Ich bin dran.");                                             //Computer fängt an zu ziehen bis mehr als 16, wenn spieler kleiner oder gleich 21 hat
+
+                    while (computergesamt < 17)
+                    {
+                        Karte = KarteZiehen();
+                        computergesamt += Karte;
+                        Console.WriteLine($"Karte: {Karte} Gesamt: {computergesamt,2}");
+                    }
+
+                    if (computergesamt > 21)                                                        //Computer verliert wenn er mehr als 21 hat
+                    {
+                        chipsguthaben = ComputerVerliert(chipsguthaben, chipseinsatz);      
+
+                    }
+                    else if (spielergesamt <= computergesamt)
+                    {
+                        ComputerGewinnt(chipsguthaben);                                             //Computer gewinnt wenn er gleich oder mehr hat also der Spieler
+                    }
+                    else
+                    {
+                        chipsguthaben = ComputerVerliert(chipsguthaben, chipseinsatz);               //Computer verliert bei weniger als der Speielr
+                    }
+
+                }
+                else
+                {
+                    ComputerGewinnt(chipsguthaben);                                                 //Computer gewinnt wenn Spieler mehr als 21 hat
+                }
+
+
+                Console.WriteLine($"Dein Guthaben beträgt: {chipsguthaben}");
+
+                if (chipsguthaben == 0)
+                {
+                    Console.WriteLine("Tut mit leid du hast keine Chips mehr.");                    //Spiel wird beendet wenn Spieler keine Chips mehr hat
+                    Laufend = false;
+                }
+                else
+                {
+
+                    Console.WriteLine("Willst du noch weiter spielen(j/n)?");                       //Abfrage für eine weitere Runde
+                    string Eingabe2 = Console.ReadLine();
+
+                    if (Eingabe2 == "N" || Eingabe2 == "n")
+                    {
+                        Laufend = false;
+                        Console.WriteLine("Ok, auf Wiedersehen.");                                  
+                    }
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
